97ce98492f11dd49a1aa076d559b16de3884187c
piyumi ranasinghe <piyumi.ranasinghe@itelasoft.com>
2020-01-02 13:14:21 +0530

    Setting React App

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "note-uploads-second-bucket"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://m88dsztgs4.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_ew4pSXTbs",
+      APP_CLIENT_ID: "5g2bougba1o8m5knchqvhe1k6g",
+      IDENTITY_POOL_ID: "us-east-2:503035e7-f9ce-498f-898c-50bc9ba13091"
+    }
+  };
